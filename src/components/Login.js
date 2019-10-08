2f9bc85f6df0c4d18a8b975163fd65231d69ed9b
Redis Rira <redis123445@gmail.com>
2019-10-08 20:26:57 +0200

    Changed

--- src/components/Login.js
+++ src/components/Login.js
@@ -23,6 +23,13 @@ export default class Login extends Component {
           fields="name,email,picture"
           callback={responseFacebook}
           />
+          <GoogleLogin
+          clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
       </div>
     );
   }
